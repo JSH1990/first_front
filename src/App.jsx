c0a00b071696f4551cceede56696aef59f1b9331
jshblack <118979741+JSH1990@users.noreply.github.com>
2025-05-08 22:09:28 +0900

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SuccessPage from "./pages/SuccessPage";
-import PropsTest from "./pages/PropsTest";
-import PropsTest2 from "./pages/PropsTest2";
+import Say from "./pages/UseStateTest";
 
 function App() {
     return (
         <Router>
             <Routes>
-                <Route path="/" element={<PropsTest2 />}/>
+                <Route path="/" element={<Say />}/>
                 <Route path="/success" element={<SuccessPage />} />
             </Routes>
         </Router>
